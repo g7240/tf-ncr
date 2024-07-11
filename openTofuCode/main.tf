81b8d572d4084188dd25c04d12042fcaeec570d8
g7240 <107947650+g7240@users.noreply.github.com>
2024-07-11 11:41:30 +0200

    S3 bucket and EC2 expose apache statically served web-page on port 80

--- openTofuCode/main.tf
+++ openTofuCode/main.tf
@@ -2,21 +2,49 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "private_bucket" {
-  bucket = "my-private-bucket-fjjdxq3o90p"
+# Create an S3 bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-bucket-fjjdxq3o90p"
   acl    = "private"
+}
 
-  tags = {
-    Name        = "My Private Bucket"
-    Environment = "Dev"
+# Create a security group to allow HTTP access
+resource "aws_security_group" "ncr_sg" {
+  name        = "ncr_sg"
+  description = "Allow HTTP traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.private_bucket.bucket
+# Create an EC2 instance to run the NCR service
+resource "aws_instance" "ncr_instance" {
+  ami           = "ami-0314c062c813a4aa0" # Choose AMI that fits your needs
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.ncr_sg.name]
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
+              echo '<html><body><h1>NCR Service</h1></body></html>' > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "NCR Instance"
+  }
 }
+
