37c2604da19d98eaa717bcbf4a6eed0b2977e2aa
g7240 <107947650+g7240@users.noreply.github.com>
2024-07-15 18:31:41 +0200

    removed and tested unnecessary network configurations

--- openTofuCode/main.tf
+++ openTofuCode/main.tf
@@ -21,20 +21,6 @@ resource "aws_security_group" "ncr_sg" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
